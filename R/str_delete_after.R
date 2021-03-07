02d27e67e0182e96735f9bc91e5907cbbb59e389
PaulESantos <paulefrens@gmail.com>
2021-03-07 18:33:44 -0500

    readme

---
+++ R/str_delete_after.R
@@ -0,0 +1,25 @@
+#' Delete characters in a string which occur after a specified pattern
+#'
+#' @description
+#' Vectorised over 'string' and 'pattern'.
+#' @param string The string from which to extract.
+#' @param pattern Pattern to look for. The default interpretation is a regular expression.
+#' @param pos Position of the character used as a marker.
+#'
+#' @return The part of the string before the pattern.
+#' @export
+#'
+
+str_delete_after <- function(string, pattern, pos = 1){
+
+  pos_pattern <- stringr::str_locate_all(string = string,
+                                         pattern = pattern)
+  start_pattern <- pos_pattern[[1]][,"start"]
+  start_pattern <- rev(start_pattern)
+  start_pattern <- start_pattern[pos]
+  string_delete <- stringr::str_sub(string = string,
+                                    start = 1 ,
+                                    end = start_pattern - 1 )
+
+  return(string_delete)
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# pesa
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/pesa)](https://CRAN.R-project.org/package=pesa)
+<!-- badges: end -->
+
+This is my personal R package where I gather the various R functions I
+have found useful in one place.
+
+## Installation
+
+You can install the released version of `pesa` from
+[GitHub](https://github.com/) with:
+
+``` r
+remotes::install_github("PaulESantos/pesa")
+```
