8dada61f9b2e40f8e8c565f2fba08ad54666d1f6
PaulESantos <paulefrens@gmail.com>
2021-04-13 20:45:21 -0500

    tnrs url

--- R/tnrs_vec.R
+++ R/tnrs_vec.R
@@ -9,7 +9,7 @@
 #' x <- c("Plantago tubuloso", "Plantago tubulosa")
 #' tnrs_vec(x)
 tnrs_vec <- function(x) {
-  tnrs <- TNRS::TNRS(x)
+  tnrs <- pesa::TNRS(x)
   dplyr::tibble(Name_submitted = x) %>%
     dplyr::left_join(tnrs,
                      by = c("Name_submitted" = "Name_submitted")
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(pesa)
-
-test_check("pesa")
