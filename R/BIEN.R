b686cd11e306f80de25e9c022d653c9a3c0ae926
PaulESantos <paulefrens@gmail.com>
2021-04-15 09:52:01 -0500

    occ

--- R/BIEN.R
+++ R/BIEN.R
@@ -4,10 +4,19 @@
 #' genus/genera  and country from the BIEN database.
 #' @param genus A single genus, or a vector of genera. Genera should be capitalized.
 #' @param country A single country or a vector of country.
-#' @template occurrence
+#' @param cultivated Return known cultivated records as well?  Default is FALSE.
+#' @param new.world NULL (The default) returns global records, TRUE returns only New World, and FALSE only Old World.
+#' @param all.taxonomy Return all taxonomic information?  This includes the raw data as well as the "scrubbed" data.
+#' @param native.status Return information on introduction status?  The default value is FALSE. A value of TRUE also returns additional information on introduction status.
+#' @param natives.only Exclude detected introduced species?  Default is TRUE.
+#' @param observation.type Return information on type of observation (i.e. specimen vs. plot)?  The default value is FALSE.
+#' @param political.boundaries Return information on political boundaries for an observation? The default value is FALSE.
+#' @param collection.info Return additional information about collection and identification? The default value is FALSE.
+#' @param ... Additional arguments passed to internal functions.
+#' @note US FIA coordinates have been fuzzed and swapped, for more details see: https://www.fia.fs.fed.us/tools-data/spatial/Policy/index.php
 #' @return Dataframe containing occurrence records for the specified genera.
 #' @examples \dontrun{
-#' BIEN_occurrence_genus_country("Vaccinium", "Peru)}
+#' BIEN_occurrence_genus_country("Vaccinium", "Peru")}
 #' @family occurrence functions
 #' @export
 BIEN_occurrence_genus_country <-function(genus,
